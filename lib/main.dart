e4f2aeae00ac848638297c5b9645b1f8d94f5a9c
antonio <antoniogomesdev@gmail.com>
2023-12-17 16:29:42 -0300

    adding theme settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,32 @@
 import 'package:expenses/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorsScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
+
 void main() {
-  runApp(const MaterialApp(home: Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+          colorScheme: kColorsScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColorsScheme.onPrimaryContainer,
+              foregroundColor: kColorsScheme.primaryContainer),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorsScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorsScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: kColorsScheme.onSecondaryContainer,
+                  fontSize: 14))),
+      home: const Expenses(),
+    ),
+  );
 }
